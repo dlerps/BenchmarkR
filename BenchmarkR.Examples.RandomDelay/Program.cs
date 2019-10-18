a7ad65470f3a18589dd3fd008648ae1bb63ae00a
Daniel Lerps <d@niel.lerps.de>
2019-10-18 20:56:19 +0200

    Add random delay example project

---
+++ BenchmarkR.Examples.RandomDelay/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using BenchmarkR.Sink.Console;
+
+namespace BenchmarkR.Examples.RandomDelay
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var benchmark = new RandomDelayBenchmark();
+            
+            var benchmarkRunner = new BenchmarkRunner(benchmark);
+            var benchmarkRun = await benchmarkRunner.Run();
+
+            benchmarkRun.WriteToConsole();
+        }
+    }
+}
---
+++ BenchmarkR.Examples.RandomDelay/RandomDelayBenchmark.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BenchmarkR.Examples.RandomDelay
+{
+    public class RandomDelayBenchmark : IBenchmark
+    {
+
+        private const int MIN_DELAY = 5;
+
+        private const int MAX_DELAY = 500;
+        
+        private readonly Random _random;
+
+        public int TotalCount => 100;
+        public int BatchSize => 4;
+
+        public string Name => "Random Delay Benchmark";
+
+        public RandomDelayBenchmark()
+        {
+            _random = new Random(DateTime.UtcNow.Millisecond);
+        }
+        
+        public async Task Execute(params string[] args)
+        {
+            var ms = _random.Next(MIN_DELAY, MAX_DELAY);
+            await Task.Delay(ms);
+        }
+    }
+}
