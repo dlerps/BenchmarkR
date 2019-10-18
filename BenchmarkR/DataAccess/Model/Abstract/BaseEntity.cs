7f467efa1b3cdce44c11917e5a10c4ce603cf06e
Daniel Lerps <d@niel.lerps.de>
2019-10-18 17:47:48 +0200

    Add project scaffolding

---
+++ BenchmarkR/DataAccess/Model/Abstract/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BenchmarkR.DataAccess.Model.Abstract
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+    }
+}
